f298e0ff5151d4df6443bf6a89190d5b8b01dfea
khalidhmd <khalidhmd@outlook.com>
2018-09-08 17:13:29 +0200

    post new user

--- server.js
+++ server.js
@@ -1,48 +1,69 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MLAB_URI ||
+    "mongodb://user1:mongodb1@ds245532.mlab.com:45532/exercisetracker"
+);
 
-app.use(cors())
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"]
+  }
+});
+const UserModel = mongoose.model("user", UserSchema);
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
+//post new user
+app.post("/api/exercise/new-user", (req, res) => {
+  UserModel.create({ username: req.body.username })
+    .then(newUser => {
+      res.send(newUser);
+    })
+    .catch(err => {
+      res.send(err.errors.username.message);
+    });
+});
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+  return next({status: 404, message: 'not found'});
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
     // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    errMessage = err.errors[keys[0]].message;
   } else {
     // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
   }
   res.status(errCode).type('txt')
     .send(errMessage)
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
